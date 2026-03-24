fe4c8ec19842bdbfc2e3c57eb7f0af79157e9859
nixpl <nixpl00@gmail.com>
2026-03-24 19:34:05 +0100

    step 3.5: add bubble sort procedure

--- 1/main.pas
+++ 1/main.pas
@@ -14,6 +14,27 @@ begin
     data[k] := Random(101);
 end;
 
+procedure PerformBubbleSort(var data: TNumbers);
+var
+  x, y, tempVal, dataLen: Integer;
+begin
+  dataLen := Length(data);
+  if dataLen <= 1 then Exit;
+  
+  for x := 0 to dataLen - 2 do
+  begin
+    for y := 0 to dataLen - x - 2 do
+    begin
+      if data[y] > data[y + 1] then
+      begin
+        tempVal := data[y];
+        data[y] := data[y + 1];
+        data[y + 1] := tempVal;
+      end;
+    end;
+  end;
+end;
+
 var
   numberList: TNumbers;
   idx: Integer;
@@ -22,6 +43,13 @@ begin
   
   GenerateRandomNumbers(numberList);
   
+  WriteLn('raw:');
+  for idx := Low(numberList) to High(numberList) do Write(numberList[idx], ' ');
+  WriteLn; WriteLn;
+  
+  PerformBubbleSort(numberList);
+  
+  WriteLn('sorted:');
   for idx := Low(numberList) to High(numberList) do Write(numberList[idx], ' ');
   WriteLn;
 end.
