9952bb3720daa01418a1524242f01ba6286f286c
nixpl <nixpl00@gmail.com>
2026-03-26 14:19:53 +0100

    step 5.0: install EasyAdmin and create dashboard with CRUD controllers

---
+++ 2/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('@EasyAdmin/page/content.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Admin Panel');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Products', 'fa fa-box', Product::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
+    }
+}
