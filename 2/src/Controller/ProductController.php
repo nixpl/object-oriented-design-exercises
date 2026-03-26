13e1200949590806dd971df2bdcc4a5b98229fe0
nixpl <nixpl00@gmail.com>
2026-03-26 11:55:59 +0100

    step 3.0: init symfony project, add Product entity and JSON CRUD controller

---
+++ 2/src/Controller/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/products')]
+class ProductController extends AbstractController
+{
+    #[Route('', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): JsonResponse
+    {
+        $products = $em->getRepository(Product::class)->findAll();
+        $data = array_map(fn($p) => ['id' => $p->getId(), 'name' => $p->getName(), 'price' => $p->getPrice()], $products);
+        return $this->json($data);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(Product $product = null): JsonResponse
+    {
+        if (!$product) return $this->json(['message' => 'Not found'], 404);
+        return $this->json(['id' => $product->getId(), 'name' => $product->getName(), 'price' => $product->getPrice()]);
+    }
+
+    #[Route('', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = new Product();
+        $product->setName($data['name'] ?? 'Nowy produkt');
+        $product->setPrice($data['price'] ?? 0.0);
+        
+        $em->persist($product);
+        $em->flush();
+        
+        return $this->json(['message' => 'Created', 'id' => $product->getId()], 201);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    public function update(Request $request, Product $product = null, EntityManagerInterface $em): JsonResponse
+    {
+        if (!$product) return $this->json(['message' => 'Not found'], 404);
+        $data = json_decode($request->getContent(), true);
+        if (isset($data['name'])) $product->setName($data['name']);
+        if (isset($data['price'])) $product->setPrice($data['price']);
+        
+        $em->flush();
+        return $this->json(['message' => 'Updated']);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(Product $product = null, EntityManagerInterface $em): JsonResponse
+    {
+        if (!$product) return $this->json(['message' => 'Not found'], 404);
+        $em->remove($product);
+        $em->flush();
+        return $this->json(['message' => 'Deleted']);
+    }
+}
